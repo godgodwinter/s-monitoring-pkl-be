3124c4c59d5e4a0a5e4186d0ab78851db53d21b2
godgodwinter <kakadz.pro@gmail.com>
2022-04-27 16:16:15 +0700

    change auth management

--- routes/babeng/admin.php
+++ routes/babeng/admin.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\Http\Controllers\admin\adminKelasController;
 use App\Http\Controllers\admin\adminTapelController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::middleware('api')->group(function () {
+Route::post('/admin/auth/login', [AuthController::class, 'login'])->name('admin.auth.login');
+// Route::middleware('api')->group(function () {
+Route::middleware('auth:api')->group(function () {
     //menu-portofolio
-    Route::post('/admin/auth/register', [AuthController::class, 'register'])->name('admin.auth.register');
-    Route::post('/admin/auth/login', [AuthController::class, 'login'])->name('admin.auth.login');
+    // Route::post('/admin/auth/register', [AuthController::class, 'register'])->name('admin.auth.register');
     Route::post('/admin/auth/logout', [AuthController::class, 'logout'])->name('admin.auth.logout');
     Route::post('/admin/auth/refresh', [AuthController::class, 'refresh'])->name('admin.auth.refresh');
     Route::post('/admin/auth/me', [AuthController::class, 'me'])->name('admin.auth.me');
@@ -21,5 +23,9 @@ Route::middleware('api')->group(function () {
     Route::put('/admin/tapel/{item}', [adminTapelController::class, 'update'])->name('admin.tapel.update');
     Route::delete('/admin/tapel/{item}', [adminTapelController::class, 'destroy'])->name('admin.tapel.destroy');
 
+    Route::get('/admin/kelas', [adminKelasController::class, 'index'])->name('admin.kelas');
+    Route::post('/admin/kelas/store', [adminKelasController::class, 'store'])->name('admin.kelas.store');
+    Route::get('/admin/kelas/{item}', [adminKelasController::class, 'edit'])->name('admin.kelas.edit');
+    Route::put('/admin/kelas/{item}', [adminKelasController::class, 'update'])->name('admin.kelas.update');
+    Route::delete('/admin/kelas/{item}', [adminKelasController::class, 'destroy'])->name('admin.kelas.destroy');
 });
-
