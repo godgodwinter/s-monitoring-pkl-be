b1821678069494cc5d4a265e40654ff13920131e
godgodwinter <kakadz.pro@gmail.com>
2022-06-01 23:19:28 +0700

    fix proses pendaftaran

--- app/Http/Controllers/siswa/siswaProfileController.php
+++ app/Http/Controllers/siswa/siswaProfileController.php
@@ -57,7 +57,7 @@ class siswaProfileController extends Controller
         if ($periksa == 'Proses Pengajuan Tempat PKL' or $periksa == 'Proses Penempatan PKL') {
         } elseif (
             $periksa == 'Proses Pemberkasan'
-            or $periksa == 'Proses Persetujuan' or $periksa == 'Disetujui'  or $periksa == 'Ditolak'
+            or $periksa == 'Proses Persetujuan'  or $periksa == 'Ditolak'
         ) {
             $getPendaftaranPrakerinProsesDetail = pendaftaranprakerin_prosesdetail::with('pendaftaranprakerin_proses')
                 ->where('siswa_id', $this->siswa_id)->whereHas('pendaftaranprakerin_proses', function ($query) {
@@ -71,6 +71,41 @@ class siswaProfileController extends Controller
 
             // $anggota = $getPendaftaranPrakerinProsesDetail->id;
             $getAnggota = pendaftaranprakerin_proses::with('pendaftaranprakerin_prosesdetail')->where('status', null)->where('tempatpkl_id', $tempatpkl->id)->where('tapel_id', Fungsi::app_tapel_aktif());
+            if ($getAnggota->first()->file) {
+                $file = $UploadDir . $getAnggota->first()->file;
+            }
+            if ($getAnggota->count() > 0) {
+                $getAnggotaFirst = $getAnggota->first();
+                $objAnggota = [];
+                foreach ($getAnggotaFirst->pendaftaranprakerin_prosesdetail as $ga) {
+                    $objAnggota['id'] = $ga->siswa_id;
+                    $objAnggota['nomeridentitas'] = $ga->siswa->nomeridentitas;
+                    $objAnggota['nama'] = $ga->siswa->nama;
+                    $objAnggota['jk'] = $ga->siswa->jk;
+                    $objAnggota['alamat'] = $ga->siswa->alamat;
+                    $objAnggota['jurusan'] = "{$ga->siswa->kelasdetail->kelas->jurusan}";
+                    $objAnggota['kelas'] = "{$ga->siswa->kelasdetail->kelas->tingkatan} {$ga->siswa->kelasdetail->kelas->jurusan} {$ga->siswa->kelasdetail->kelas->suffix}";
+                    // $objAnggota['kelas'] = $ga->siswa->kelas->tingkatan + ' ' + $ga->siswa->kelas->jurusan + ' ' + $ga->siswa->kelas->suffix;
+                    // $objAnggota['nama'] = $ga->pendaftaranprakerin_prosesdetail->siswa;
+                    // array push
+                    array_push($anggota, $objAnggota);
+                }
+            }
+        } elseif (
+            $periksa == 'Disetujui'
+        ) {
+            $getPendaftaranPrakerinProsesDetail = pendaftaranprakerin_prosesdetail::with('pendaftaranprakerin_proses')
+                ->where('siswa_id', $this->siswa_id)->whereHas('pendaftaranprakerin_proses', function ($query) {
+                    $query->where('tapel_id', Fungsi::app_tapel_aktif());
+                })
+                ->first();
+            $tgl_penempatan = $getPendaftaranPrakerinProsesDetail->pendaftaranprakerin_proses ?
+                Fungsi::carbonCreatedAt($getPendaftaranPrakerinProsesDetail->pendaftaranprakerin_proses->created_at) : null;
+            $tempatpkl = $getPendaftaranPrakerinProsesDetail->pendaftaranprakerin_proses->tempatpkl ? $getPendaftaranPrakerinProsesDetail->pendaftaranprakerin_proses->tempatpkl : null;
+
+
+            // $anggota = $getPendaftaranPrakerinProsesDetail->id;
+            $getAnggota = pendaftaranprakerin_proses::with('pendaftaranprakerin_prosesdetail')->where('tempatpkl_id', $tempatpkl->id)->where('tapel_id', Fungsi::app_tapel_aktif());
             if ($getAnggota->first()->file) {
                 $file = $UploadDir . $getAnggota->first()->file;
             }
